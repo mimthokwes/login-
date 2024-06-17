104ccfbb0a4b2b9f4f594448a8e23c2627351632
mimthokwes <laanalpha@gmail.com>
2024-06-17 12:49:52 +0700

    membuat login web
    ini adalah tampilan awal dari web login "WICE" yaitu website untuk para wibu yang ingin menonton anime dengan gratis awkwowkaowk

---
+++ codingan hamim/script.js
@@ -0,0 +1,14 @@
+document.getElementById('registerLink').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('loginBox').style.display = 'none';
+    document.getElementById('registerBox').style.display = 'block';
+});
+
+document.getElementById('loginLink').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('registerBox').style.display = 'none';
+    document.getElementById('loginBox').style.display = 'block';   
+});
+
+
+
